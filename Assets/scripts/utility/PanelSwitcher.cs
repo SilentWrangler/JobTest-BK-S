4f32dfa771644ccd7a3244fefec8b41fbf6b61f5
SilentWrangler <thousandgoblins@gmail.com>
2021-03-20 13:40:16 +0300

    UI & Calculator update

---
+++ Assets/scripts/utility/PanelSwitcher.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.Events;
+
+public class PanelSwitcher : MonoBehaviour
+{
+  [System.Serializable]
+  public struct Switch{
+    public UnityEvent enter_callback;
+    public UnityEvent exit_callback;
+  }
+
+  public Switch[] panels;
+  private int current = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panels[0].enter_callback.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public void SwitchPanel(int index){
+      if (index==current) return;
+      panels[index].enter_callback.Invoke();
+      panels[current].exit_callback.Invoke();
+      current = index;
+
+    }
+}
